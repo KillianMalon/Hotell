0f3873af120d1138a3d63106901d85d4afb4d533
KillianMalon <76114286+KillianMalon@users.noreply.github.com>
2021-04-23 09:55:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 $index = 1;
 require_once './component/header.php';
+
+//Formulaire de recherche de disponibilitÃ© des chambres
+require_once './component/searchform.php'
 ?>
-</body>
-</html>
